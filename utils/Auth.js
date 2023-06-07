892d4aa137cbd5f2da35bb899a227305bf7208ed
Bayram Bayraktar <105519834+BayramBayraktar@users.noreply.github.com>
2023-06-07 14:39:37 +0300

    Update Auth.js

--- utils/Auth.js
+++ utils/Auth.js
@@ -1,6 +1,7 @@
 
 export const isAuth = async (cookie) => {
     if (cookie.session) {
+        console.log(cookie)
         const data = await fetch(`/api/auth/check`, {
             headers: {
                 Authorization: `Bearer ${cookie.session}`
