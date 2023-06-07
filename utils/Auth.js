15c5dec8132c193bf441546ae8f63d04b3629327
Bayram Bayraktar <105519834+BayramBayraktar@users.noreply.github.com>
2023-06-07 15:11:48 +0300

    Update Auth.js

--- utils/Auth.js
+++ utils/Auth.js
@@ -1,7 +1,7 @@
 
 export const isAuth = async (cookie) => {
     if (cookie.session) {
-        const data = await fetch(`/api/auth/check`, {
+        const data = await fetch(`https://next-1-phi.vercel.app/api/auth/check`, {
             headers: {
                 Authorization: `Bearer ${cookie.session}`
             }
