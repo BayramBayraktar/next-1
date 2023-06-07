7e1180f482bad1e1de8365a723c902d44a57fdf9
Bayram Bayraktar <105519834+BayramBayraktar@users.noreply.github.com>
2023-06-07 14:21:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,19 @@ const nextConfig = {
     API_URL: process.env.API_URL,
     SECRET_KEY_JWT: process.env.SECRET_KEY_JWT,
     TOKEN_HEADER_KEY: process.env.TOKEN_HEADER_KEY
+  },
+   async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 
 }
